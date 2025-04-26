339d5360d271c9dc8793316401dd26f1e84bbf75
jared <jaredrotich3@gmail.com>
2025-04-26 23:14:05 +0300

    update congested files i filled

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import Navbar from './components/Navbar';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+    </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

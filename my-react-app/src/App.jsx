e9403910b300e46d4dc044de1c6ade513c815d9e
jared <jaredrotich3@gmail.com>
2025-04-27 16:29:14 +0300

    added css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,15 +4,23 @@ import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import Navbar from './components/Navbar';
 import './App.css';
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import { CartProvider } from "./context/CartContext";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path="/product/:id" element={<ProductPage />} />
-    </Routes>
+     <CartProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+      </Router>
+    </CartProvider>
     </>
   );
 };

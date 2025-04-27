03c134cf95e71bed0ddd2b1dc88079b7671a2cb1
jared <jaredrotich3@gmail.com>
2025-04-27 11:42:40 +0300

    made adjustments on home file

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import productCard from './ProductCard';
+import ProductCard from './ProductCard';
+import SearchBar from './SearchBar';
+import CategoryFilter from './CategoryFilter';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [categories, setCategories] =useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3000/products')
         .then((res) => res.json())
-        .then((data) => setProducts(data))
-        .catch((err) => console.error('Failed to fetch products', err));
-    }, []);
+        .then((data) => {
+            setProducts(data);
+            setFilteredProducts(data);
+            const allCategories = [...new Set(data.map(item => item.category))];
+            setCategories(allCategories);
+          })
+          .catch((err) => console.error('Failed to fetch products', err));
+      }, []);
+    
+      const handleSearch = (query) => {
+        const filtered = products.filter((product) =>
+          product.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setFilteredProducts(filtered);
+      };
+    
+      const handleCategoryFilter = (category) => {
+        if (category === "All") {
+          setFilteredProducts(products);
+        } else {
+          const filtered = products.filter((product) => product.category === category);
+          setFilteredProducts(filtered);
+        }
+      };
 
     return (
         <div className="home-container">
             <h1>Our Products</h1>
-            <div className='product-grid'>
-                {products.map((product) => (
-                    <productCard key={product.id} product={product} />
-                ))}
-
-            </div>
+            <SearchBar onSearch={handleSearch} />
+      <CategoryFilter categories={categories} onFilter={handleCategoryFilter} />
+      <div className="product-grid">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
         </div>
     );
 };

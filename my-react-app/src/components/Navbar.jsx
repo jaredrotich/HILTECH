3661659decaeba55385fa3b8a71e6db3e0544617
jared <jaredrotich3@gmail.com>
2025-04-27 11:15:59 +0300

    declared cart presence in navbar

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
 import './Navbar.css';
 
 const Navbar = () => {
+    const { cart } = useContext(CartContext);
+
     return (
         <nav className='navbar'>
          <div className='logo'>
            <Link to="/">MyShop</Link>
+         </div>
+         <div className='cart'>
+          🛒 Cart ({cart.length})
          </div>
         </nav>
     );

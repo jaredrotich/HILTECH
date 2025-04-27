1ea7c62cbaec81ff7cda2558562ae43151e72899
jared <jaredrotich3@gmail.com>
2025-04-27 16:05:47 +0300

    added product card functionalities

--- my-react-app/src/components/ProductCard.jsx
+++ my-react-app/src/components/ProductCard.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from '../context/CartContext';
 
 const ProductCard = ({ product }) => {
+    const { addToCart } = useContext(CartContext);
+
     return (
         <div className="product-card">
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>ksh {product.price.toLocaleString()}</p>
+            <div className="buttons">
             <Link to={'/product/&{product.id}'}>
               <button>View Details</button>
             </Link>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+            </div>
         </div>
     );
 };

32c2cac2c911ece6541e8a745cc6da851af9a7bf
jared <jaredrotich3@gmail.com>
2025-05-13 22:07:09 +0300

    worked on NavBar and added complex files

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,18 +1,56 @@
+// import React from 'react';
+// import "./Navbar.css";
+
+// const Navbar = () => {
+//     return (
+//         <nav className='navbar'>
+//             <div className='navbar__logo'>HILTECH</div>
+//             <ul className='navbar__menu'>
+//             <li><a href='/'>Home</a></li>
+//                 <li><a href='/'>Products</a></li>
+//                 <li><a href='/'>Contact</a></li>
+//             </ul>
+//         </nav>
+//     );
+// };
+
+
+// export default Navbar;
+
 import React from 'react';
+import {  NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
-            <div className='navbar__logo'>HILTECH</div>
-            <ul className='navbar__menu'>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/'>Products</a></li>
-                <li><a href='/'>Contact</a></li>
-            </ul>
+        <nav>
+          <h2>HILTECH</h2>
+          <NavLink
+          to="/"
+          className="nav-link"
+          >
+            Home
+          </NavLink>
+          <NavLink
+          to="/about"
+          className="nav-link"
+          >
+            About
+          </NavLink>
+          <NavLink
+          to="/contact"
+          className="nav-link"
+          >
+            Contact
+          </NavLink>
+          <NavLink
+          to="/projects"
+          className="nav-link"
+          >
+            Products
+          </NavLink>
         </nav>
-    );
-};
-
+    )
+}
 
 export default Navbar;
---
+++ my-react-app/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+ function Home() {
+    return (
+        <section className="home">
+            <h1>Hi, I'm Jared  ⚔️ @The Backend Nightmare 🤺</h1>
+            <p>A passionate Code Warrior crafting clean & powerful web experiences.</p>
+        </section>
+    );
+}
+
+export default HomePage;
